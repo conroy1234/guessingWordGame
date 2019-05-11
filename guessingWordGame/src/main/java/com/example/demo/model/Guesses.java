6eccd63bb3f886059216837d125259a85cd22b65
conroy <conroyaw@gmail.com>
2019-05-11 17:08:33 +0100

    write integrition test that put all together

--- guessingWordGame/src/main/java/com/example/demo/model/Guesses.java
+++ guessingWordGame/src/main/java/com/example/demo/model/Guesses.java
@@ -1,12 +1,15 @@
 package com.example.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Guesses {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String guesses;
 	private int numberofSearches;	
--- guessingWordGame/src/test/java/com/example/demo/GuessingWordGameApplicationTests.java
+++ guessingWordGame/src/test/java/com/example/demo/GuessingWordGameApplicationTests.java
@@ -1,16 +1,72 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import com.example.demo.model.Secret;
+import com.example.demo.model.Guesses;
+import com.example.demo.repository.GuessesRepository;
+import com.example.demo.repository.SecretRepository;
+import com.example.demo.model.Secret;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = GuessingWordGameApplication.class)
+@WebAppConfiguration
 public class GuessingWordGameApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	WebApplicationContext context;
+
+	@Autowired
+	GuessesRepository guessesRepository;
+
+	@Autowired
+	SecretRepository secretRepository;
+
+	MockMvc mockMvc;
+
+	Guesses entry;
+	List<Guesses> entries;
+	List<Secret> secrets;
+	Secret Secret;
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		 entry = guessesRepository.save(new Guesses("SECRETWORD",9,"SECRETWORD"));
+		 
+		 entries =(List<Guesses>) guessesRepository.findAll();
+		 secrets = (List<Secret>) secretRepository.findAll();
 	}
 
+	@Test
+	public void persistsGuestbookEntry() {
+		int expected = 2;
+		assertEquals(expected, entries.size());
+	}
+	
+
+	@Test
+	public void contextLoads() throws Exception {
+
+		mockMvc.perform(get("/api/search/SECRETWORD/SECRETWORD")).andExpect(status().isOk());
+
+	}
 }
