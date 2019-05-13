455b2fa6b1778ba8a1421aae9219365f74833c0e
conroy <conroyaw@gmail.com>
2019-05-13 10:45:28 +0100

    s

--- guessingWordGame/src/test/java/com/example/demo/GuessingWordGameApplicationTests.java
+++ guessingWordGame/src/test/java/com/example/demo/GuessingWordGameApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.List;
 
@@ -14,18 +15,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import com.example.demo.model.Secret;
+
 import com.example.demo.model.Guesses;
+import com.example.demo.model.Secret;
 import com.example.demo.repository.GuessesRepository;
 import com.example.demo.repository.SecretRepository;
-import com.example.demo.model.Secret;
+import com.example.demo.service.GuessesService;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = GuessingWordGameApplication.class)
 @WebAppConfiguration
@@ -42,18 +43,23 @@ public class GuessingWordGameApplicationTests {
 
 	MockMvc mockMvc;
 
+	GuessesService guessesService = mock(GuessesService.class);
+	Guesses guesses = mock(Guesses.class);
+
 	Guesses entry;
 	List<Guesses> entries;
 	List<Secret> secrets;
 	Secret Secret;
+	String incorrectSearch = "search";
+	String correctSearch = "SECRETWORD";
 
 	@Before
 	public void setUp() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-		 entry = guessesRepository.save(new Guesses("SECRETWORD",9,"SECRETWORD"));
-		 
-		 entries =(List<Guesses>) guessesRepository.findAll();
-		 secrets = (List<Secret>) secretRepository.findAll();
+		entry = guessesRepository.save(new Guesses("SECRETWORD", 9, "SECRETWORD"));
+
+		entries = (List<Guesses>) guessesRepository.findAll();
+		secrets = (List<Secret>) secretRepository.findAll();
 	}
 
 	@Test
@@ -61,7 +67,6 @@ public class GuessingWordGameApplicationTests {
 		int expected = 2;
 		assertEquals(expected, entries.size());
 	}
-	
 
 	@Test
 	public void contextLoads() throws Exception {
@@ -69,4 +74,28 @@ public class GuessingWordGameApplicationTests {
 		mockMvc.perform(get("/api/search/SECRETWORD/SECRETWORD")).andExpect(status().isOk());
 
 	}
+
+	@Test
+	public void testMultiPlayersIncorrect() {
+		Secret secret = new Secret();
+		String searchWord = secretRepository.findBysearchWord(incorrectSearch);
+		secret.setSearchWord(searchWord);
+		guessesService.search(secret.getSearchWord());
+
+		when(guessesService.search(incorrectSearch)).thenReturn(guesses).thenReturn(guesses).thenReturn(guesses)
+				.thenReturn(guesses).thenReturn(guesses).thenReturn(guesses);
+		assertNull(secret.getSearchWord());
+	}
+
+	@Test
+	public void testcorrectSearch() {
+		Secret secret = new Secret();
+		String searchWord = secretRepository.findBysearchWord(correctSearch);
+		secret.setSearchWord(searchWord);
+		guessesService.search(secret.getSearchWord());
+		when(guessesService.search(correctSearch)).thenReturn(guesses);
+		assertNotNull(secret.getSearchWord());
+		assertEquals(searchWord, secret.getSearchWord());
+	}
+
 }
